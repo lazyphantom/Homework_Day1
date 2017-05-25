f6b64b02ecd7cc57c269ac0840191319ebf1d5bd
shihjie shyu <shih_jie@outlook.com>
2017-05-25 21:02:52 +0800

    upload

---
+++ Homework_Day1/Homework_Day1/Service.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Day1
+{
+    public class Service
+    {
+        private IProductManager _procudtManager;
+
+        public Service()
+        {
+            this._procudtManager = new ProductManager();
+        }
+        public Service(IProductManager productManager)
+        {
+            this._procudtManager = productManager;
+        }
+
+        public string getSplitSum(string name, int num)
+        {
+            IProductManager productManager = _procudtManager;
+            List<int> list = productManager.GetListByName(name);
+            int temp_int = 0;
+            StringBuilder temp_string = new StringBuilder();
+            int times = 0;
+
+            foreach (int item in list)
+            {
+                temp_int += item;
+                if (times + 1 == list.Count())
+                {
+                    temp_string.Append(temp_int);
+                }
+                if ((times + 1) % num == 0)
+                {
+                    temp_string.Append(temp_int + ",");
+                    temp_int = 0;
+                }
+                times++;
+
+            }
+
+            return temp_string.ToString();
+        }
+    }
+
+    public interface IProductManager
+    {
+        List<int> GetListByName(string name);
+    }
+
+    public class ProductManager : IProductManager
+    {
+        public List<int> GetListByName(string name)
+        {
+            switch (name)
+            {
+                case "Id":
+                    return (from data in ProductList.GetProductList() select data.Id).ToList();
+                case "Cost":
+                    return (from data in ProductList.GetProductList() select data.Cost).ToList();
+                case "Revenue":
+                    return (from data in ProductList.GetProductList() select data.Revenue).ToList();
+                case "SellPrice":
+                    return (from data in ProductList.GetProductList() select data.SellPrice).ToList();
+                default:
+                    return new List<int>() { 0 };
+            }
+        }
+    }
+
+    public static class ProductList
+    {
+
+        private static List<Product> context;
+        static ProductList()
+        {
+            context = new List<Product>() {
+                new Product(1, 1, 11, 21),
+                new Product(2, 2, 12, 22),
+                new Product(3, 3, 13, 23),
+                new Product(4, 4, 14, 24),
+                new Product(5, 5, 15, 25),
+                new Product(6, 6, 16, 26),
+                new Product(7, 7, 17, 27),
+                new Product(8, 8, 18, 28),
+                new Product(9, 9, 19, 29),
+                new Product(10, 10, 20, 30),
+                new Product(11, 11, 21, 31)
+            };
+        }
+        public static List<Product> GetProductList()
+        {
+            return context;
+        }
+    }
+
+    public struct Product
+    {
+        public int Id { get; private set; }
+        public int Cost { get; private set; }
+        public int Revenue { get; private set; }
+        public int SellPrice { get; private set; }
+
+        public Product(int id, int cost, int revenue, int sellprice)
+        {
+            Id = id;
+            Cost = cost;
+            Revenue = revenue;
+            SellPrice = sellprice;
+        }
+    }
+}
---
+++ Homework_Day1/Homework_Day1Tests/ServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Homework_Day1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+
+namespace Homework_Day1.Tests
+{
+    [TestClass()]
+    public class ServiceTests
+    {
+        [TestMethod()]
+        public void Sample1_3筆一組取Cost總和()
+        {
+            var productManager = Substitute.For<IProductManager>();
+            string name = "Cost";
+            productManager.GetListByName(name).Returns(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 });
+            var expected = "6,15,24,21";
+
+            var target = new Service(productManager);
+
+            var actual = target.getSplitSum(name, 3);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Sample2_4筆一組取Revenue總和()
+        {
+            var productManager = Substitute.For<IProductManager>();
+            string name = "Revenue";
+            productManager.GetListByName(name).Returns(new List<int> { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 });
+            var expected = "50,66,60";
+
+            var target = new Service(productManager);
+
+            var actual = target.getSplitSum(name, 4);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
