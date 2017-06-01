737cdee27f3cc4a5039e31cdf1d359cada0b3f77
shihjie shyu <shih_jie@outlook.com>
2017-06-01 19:18:39 +0800

    update 1. 更改class name "Service"-> SplitSumService、 "ServiceTest"->"SplitSumServiceTests" 2. 拿掉假物件 3. 把ProductList移到 UnitTest 4. 回傳string改為List<int>

--- Homework_Day1/Homework_Day1/Service.cs
+++ Homework_Day1/Homework_Day1/Service.cs
@@ -6,25 +6,32 @@ using System.Threading.Tasks;
 
 namespace Homework_Day1
 {
-    public class Service
+    public class SplitSumService
     {
-        private IProductManager _procudtManager;
+        public List<int> GetSplitSum(List<Product> productList, string name, int num)
+        {
+            List<int> list;
 
-        public Service()
-        {
-            this._procudtManager = new ProductManager();
-        }
-        public Service(IProductManager productManager)
-        {
-            this._procudtManager = productManager;
-        }
-
-        public string getSplitSum(string name, int num)
-        {
-            IProductManager productManager = _procudtManager;
-            List<int> list = productManager.GetListByName(name);
+            switch (name)
+            {
+                case "Id":
+                    list = (from data in productList select data.Id).ToList();
+                    break;
+                case "Cost":
+                    list = (from data in productList select data.Cost).ToList();
+                    break;
+                case "Revenue":
+                    list = (from data in productList select data.Revenue).ToList();
+                    break;
+                case "SellPrice":
+                    list = (from data in productList select data.SellPrice).ToList();
+                    break;
+                default:
+                    list = new List<int>();
+                    break;
+            }
             int temp_int = 0;
-            StringBuilder temp_string = new StringBuilder();
+            List<int> temp_retune = new List<int>();
             int times = 0;
 
             foreach (int item in list)
@@ -32,73 +39,21 @@ namespace Homework_Day1
                 temp_int += item;
                 if (times + 1 == list.Count())
                 {
-                    temp_string.Append(temp_int);
+                    temp_retune.Add(temp_int);
                 }
                 if ((times + 1) % num == 0)
                 {
-                    temp_string.Append(temp_int + ",");
+                    temp_retune.Add(temp_int);
                     temp_int = 0;
                 }
                 times++;
-
             }
 
-            return temp_string.ToString();
+            return temp_retune;
         }
     }
 
-    public interface IProductManager
-    {
-        List<int> GetListByName(string name);
-    }
-
-    public class ProductManager : IProductManager
-    {
-        public List<int> GetListByName(string name)
-        {
-            switch (name)
-            {
-                case "Id":
-                    return (from data in ProductList.GetProductList() select data.Id).ToList();
-                case "Cost":
-                    return (from data in ProductList.GetProductList() select data.Cost).ToList();
-                case "Revenue":
-                    return (from data in ProductList.GetProductList() select data.Revenue).ToList();
-                case "SellPrice":
-                    return (from data in ProductList.GetProductList() select data.SellPrice).ToList();
-                default:
-                    return new List<int>() { 0 };
-            }
-        }
-    }
-
-    public static class ProductList
-    {
-
-        private static List<Product> context;
-        static ProductList()
-        {
-            context = new List<Product>() {
-                new Product(1, 1, 11, 21),
-                new Product(2, 2, 12, 22),
-                new Product(3, 3, 13, 23),
-                new Product(4, 4, 14, 24),
-                new Product(5, 5, 15, 25),
-                new Product(6, 6, 16, 26),
-                new Product(7, 7, 17, 27),
-                new Product(8, 8, 18, 28),
-                new Product(9, 9, 19, 29),
-                new Product(10, 10, 20, 30),
-                new Product(11, 11, 21, 31)
-            };
-        }
-        public static List<Product> GetProductList()
-        {
-            return context;
-        }
-    }
-
-    public struct Product
+    public class Product
     {
         public int Id { get; private set; }
         public int Cost { get; private set; }
--- Homework_Day1/Homework_Day1Tests/ServiceTests.cs
+++ Homework_Day1/Homework_Day1Tests/ServiceTests.cs
@@ -10,34 +10,60 @@ using NSubstitute;
 namespace Homework_Day1.Tests
 {
     [TestClass()]
-    public class ServiceTests
+    public class SplitSumServiceTests
     {
         [TestMethod()]
         public void Sample1_3筆一組取Cost總和()
         {
-            var productManager = Substitute.For<IProductManager>();
+            var productList = new ProductList().GetProductList();
             string name = "Cost";
-            productManager.GetListByName(name).Returns(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 });
-            var expected = "6,15,24,21";
+            int groupCount = 3;
 
-            var target = new Service(productManager);
+            var expected = new List<int>() { 6, 15, 24, 21 };
 
-            var actual = target.getSplitSum(name, 3);
-            Assert.AreEqual(expected, actual);
+            var actual = new SplitSumService().GetSplitSum(productList, name, groupCount);
+            CollectionAssert.AreEqual(expected, actual);
         }
-
+        
         [TestMethod()]
         public void Sample2_4筆一組取Revenue總和()
         {
-            var productManager = Substitute.For<IProductManager>();
+            var productList = new ProductList().GetProductList();
             string name = "Revenue";
-            productManager.GetListByName(name).Returns(new List<int> { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 });
-            var expected = "50,66,60";
+            int groupCount = 4;
 
-            var target = new Service(productManager);
+            var expected = new List<int>() { 50, 66, 60 };
 
-            var actual = target.getSplitSum(name, 4);
-            Assert.AreEqual(expected, actual);
+            var actual = new SplitSumService().GetSplitSum(productList, name, groupCount);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        
+    }
+
+    public class ProductList
+    {
+        private static List<Product> context;
+
+        public ProductList()
+        {
+            context = new List<Product>() { new Product(1, 1, 11, 21),
+                                            new Product(2, 2, 12, 22),
+                                            new Product(3, 3, 13, 23),
+                                            new Product(4, 4, 14, 24),
+                                            new Product(5, 5, 15, 25),
+                                            new Product(6, 6, 16, 26),
+                                            new Product(7, 7, 17, 27),
+                                            new Product(8, 8, 18, 28),
+                                            new Product(9, 9, 19, 29),
+                                            new Product(10, 10, 20, 30),
+                                            new Product(11, 11, 21, 31)};
+        }
+
+        public List<Product> GetProductList()
+        {
+            return context;
         }
     }
+
+
 }
